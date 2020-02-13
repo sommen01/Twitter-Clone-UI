e7d7cfc130a8f43479696c03b2448259bc2fcdf5
Luka Lickah <lukamatheus0@gmail.com>
2020-02-12 20:44:28 -0400

    timeline

--- lib/src/pages/Home.dart
+++ lib/src/pages/Home.dart
@@ -45,7 +45,134 @@ class HomePage extends StatelessWidget {
       ),
       
       body: Container(     
-       
+          color : Color(0xff121c26),
+          child: ListView(
+              children:<Widget>[
+                Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.16,
+                    child: Row(
+                      children: <Widget>[
+                        Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: <Widget>[
+                             Padding(
+                               padding: const EdgeInsets.all(15),
+                               child: Container(
+                                 height: 65,
+                                 width: 65,
+                                      decoration: new BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.white,
+                                      image: const DecorationImage(
+                                      image: AssetImage('assets/image_icon.jpeg'),
+                                      fit: BoxFit.cover,
+                                     ),
+                                   ),
+                                 ),
+                             ),
+                            ],
+                          ),
+                          //   Padding(
+                          //     padding: const EdgeInsets.only(bottom : 110.0),
+                          //     child: Row(children: <Widget>[
+                          //       Text('Joy', style: TextStyle(color: Colors.white, fontSize: 20),
+                          //       ),
+                          //       SizedBox(
+                          //         width: 5,
+                          //       ),
+                          //       Text('@lickahluka', style: TextStyle(color : Colors.grey, fontSize: 20),),
+                          //     ],
+                          // ),
+                          //   ),
+                         Container(
+                           height: 200,
+                           width: 330,
+                           child: Column(
+                             children: <Widget>[
+                               SizedBox(
+                                 height: 15,
+                               ),
+                               Container(
+                                    child: Row(children: <Widget>[
+                                      Text('Joy', style: TextStyle(color: Colors.white, fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text('@lickahluka', style: TextStyle(color : Colors.grey, fontSize: 20),),
+                                      SizedBox(
+                                        width: 120,
+                                      ),
+                                      Text(' - 1h', style: TextStyle(color : Colors.grey, fontSize: 20),),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Icon(FontAwesomeIcons.chevronDown, color: Colors.grey, size: 13,),
+                                    ],
+                                ),
+                               ),
+                               SizedBox(
+                                 height : 1
+                               ),
+                                Container(
+                                child: Column(
+                                  children: <Widget>[
+                                    Row(children: <Widget>[
+                                      Text('Hoje eu clonei o layout do twitter\nutilizando o flutter', style: TextStyle(color: Colors.white, fontSize: 19),
+                                      ),
+                                    ],
+                                    ),
+                                    SizedBox(
+                                      height: 22,
+                                    ),
+                                     Row(children: <Widget>[
+                                      Icon(FontAwesomeIcons.comment, color: Colors.white, size: 18,),
+                                      SizedBox(
+                                        width: 70,
+                                      ),
+                                      Icon(FontAwesomeIcons.retweet, color: Colors.white, size: 20,),
+                                      SizedBox(
+                                        width: 70,
+                                      ),
+                                      Icon(FontAwesomeIcons.heart, color: Colors.white, size: 18,),
+                                      SizedBox(
+                                        width: 70,
+                                      ),
+                                      Icon(FontAwesomeIcons.shareAlt, color: Colors.white, size: 18,),
+
+                                    ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                             ],
+                           ),
+                         ),
+                        ],
+                       ),
+                    ),
+                    
+                  // Container(
+                  //   width: double.infinity,
+                  //   height: MediaQuery.of(context).size.height * 0.20,
+                  //   child: Padding(
+                  //     padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  //     child: Card(
+                  //       color: Colors.white,  
+                  //         shape: RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.circular(15.0),
+                  //       ),
+                                            
+                  //     ),
+                  //   ),
+                  // ),
+                   Divider(
+                     color: Colors.grey,
+                     thickness: 0.3,
+                   ),
+            ],
+          ),
         ),
     );
   }
